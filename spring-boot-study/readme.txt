af36c9f273f6d0a549cf3a6bd463419d5d1952d3
bage2014 <893542907@qq.com>
2018-05-21 22:12:44 +0800

    增加jdbc功能、但是比如Redis、Neo4j未完成

---
+++ spring-boot-study/readme.txt
@@ -0,0 +1,6 @@
+
+redis 暂时未验证
+rabbitMQ 暂时未验证
+Neo4j 暂时未验证
+
+
---
+++ spring-boot-study/spring-boot-study-rabbitmq/src/main/java/com/bage/spring/boot/study/rabbitmq/App.java
@@ -0,0 +1,13 @@
+package com.bage.spring.boot.study.rabbitmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
